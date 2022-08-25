73913fd344b3ad68e52880ebc0540a835635cdfc
Alvin Daniel <alvindx8@gmail.com>
2022-08-25 19:16:37 +0800

    pick image from gallery and snap from camera

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,21 +1,111 @@
-import { View, Text, TouchableOpacity, Button } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  StyleSheet,
+  Image,
+  StatusBar,
+} from "react-native";
 import React, { useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import { useNavigation } from "@react-navigation/native";
 import { TextInput } from "react-native-paper";
 import useAuth from "../hooks/useAuth";
+import { Camera } from "expo-camera";
+import * as ImagePicker from "expo-image-picker";
+import { Menu, MenuItem, MenuDivider } from "react-native-material-menu";
 
 const RegistrationScreen = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
   const ref_input_last_name = useRef();
   const ref_input_email = useRef();
   const ref_input_password = useRef();
   const navigation = useNavigation();
   const { registerUser } = useAuth();
+  const [menuVisibility, setMenuVisibility] = useState(false);
+  const [startCamera, setStartCamera] = useState(false);
+  let cameraRef = useRef();
+
+  let chooseFromGallery = async () => {
+    setMenuVisibility(false);
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  const openCamera = async () => {
+    setMenuVisibility(false);
+    let cameraPermission = await Camera.requestCameraPermissionsAsync();
+
+    if (cameraPermission.granted === false) {
+      alert("Permission to access camera is required!");
+      return;
+    }
+    setStartCamera(true);
+  };
+
+  let captureImage = async () => {
+    let newPhoto = await cameraRef.current.takePictureAsync(null);
+    setStartCamera(false);
+    setSelectedImage({ localUri: newPhoto.uri });
+  };
+
+  if (startCamera) {
+    return (
+      <Camera style={{ flex: 1, width: "100%" }} ref={cameraRef}>
+        <View
+          style={{
+            position: "absolute",
+            bottom: 0,
+            flexDirection: "row",
+            flex: 1,
+            width: "100%",
+            padding: 20,
+            justifyContent: "space-between",
+          }}
+        >
+          <View
+            style={{
+              alignSelf: "center",
+              flex: 1,
+              alignItems: "center",
+            }}
+          >
+            <TouchableOpacity
+              onPress={captureImage}
+              style={{
+                width: 70,
+                height: 70,
+                bottom: 0,
+                borderRadius: 50,
+                backgroundColor: "#fff",
+              }}
+            />
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </Camera>
+    );
+  }
 
   return (
     <SafeAreaView className="p-2 bg-white flex-1">
@@ -28,7 +118,7 @@ const RegistrationScreen = () => {
           <View></View>
         </View>
       </View>
-      <View className="p-2 pt-10 space-y-4">
+      <View className="p-2 pt-10 space-y-5">
         <View className="flex-row space-x-4">
           <View className="flex-1 w-full">
             <TextInput
@@ -93,6 +183,43 @@ const RegistrationScreen = () => {
             ref={ref_input_password}
           />
         </View>
+        <View>
+          <View className="relative">
+            {selectedImage ? (
+              <Image
+                source={{ uri: selectedImage.localUri }}
+                style={styles.logo}
+                className="rounded-full self-center"
+              />
+            ) : (
+              <Image
+                source={require("../assets/profile_picture.png")}
+                style={styles.logo}
+                className="rounded-full self-center"
+              />
+            )}
+            <View className="absolute bottom-4 right-20">
+              <Menu
+                visible={menuVisibility}
+                anchor={
+                  <TouchableOpacity
+                    className="aspect-square rounded-full bg-green-600 items-center"
+                    onPress={() => setMenuVisibility(true)}
+                  >
+                    <MaterialIcons name="add" size={50} color="white" />
+                  </TouchableOpacity>
+                }
+                onRequestClose={() => setMenuVisibility(false)}
+              >
+                <MenuItem onPress={chooseFromGallery}>
+                  Choose From Gallery
+                </MenuItem>
+                <MenuDivider />
+                <MenuItem onPress={openCamera}>Take A Picture</MenuItem>
+              </Menu>
+            </View>
+          </View>
+        </View>
         <View className="pt-10">
           <Button
             color="#FFA500"
@@ -104,5 +231,12 @@ const RegistrationScreen = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 240,
+    height: 240,
+  },
+});
 
 export default RegistrationScreen;
